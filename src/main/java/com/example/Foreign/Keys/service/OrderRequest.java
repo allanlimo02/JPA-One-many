3bb778dfbd8e611f0e749924978577f457ff3b63
allanlimo02 <allanlimo02@gmail.com>
2022-12-14 12:56:40 +0300

    Added custom jpa Querry

---
+++ src/main/java/com/example/Foreign/Keys/service/OrderRequest.java
@@ -0,0 +1,15 @@
+package com.example.Foreign.Keys.service;
+
+import com.example.Foreign.Keys.entity.Customer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class OrderRequest {
+    private Customer customer;
+}
