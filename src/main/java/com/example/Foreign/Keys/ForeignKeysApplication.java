4fdcdd27362d0527879dbe59ff63d0d82fd29062
allanlimo02 <allanlimo02@gmail.com>
2022-12-14 12:55:03 +0300

    Added custom jpa Querry

---
+++ src/main/java/com/example/Foreign/Keys/ForeignKeysApplication.java
@@ -0,0 +1,13 @@
+package com.example.Foreign.Keys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeignKeysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForeignKeysApplication.class, args);
+	}
+
+}
