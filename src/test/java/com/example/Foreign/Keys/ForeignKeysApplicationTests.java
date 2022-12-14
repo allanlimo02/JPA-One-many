95d33e99f2a515a9dda661f0ab6accd04eea855c
allanlimo02 <allanlimo02@gmail.com>
2022-12-14 12:55:13 +0300

    Added custom jpa Querry

---
+++ src/test/java/com/example/Foreign/Keys/ForeignKeysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Foreign.Keys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeignKeysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
